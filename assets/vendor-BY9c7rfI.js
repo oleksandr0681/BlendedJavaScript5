9bd7ca1c9a1931df2f4d93e4c2cb85a807c52ed6
oleksandr0681 <72039105+oleksandr0681@users.noreply.github.com>
2026-04-25 10:46:01 +0000

    Deploying to gh-pages from @ oleksandr0681/BlendedJavaScript5@5217910875f33ca16f6a6127de05d95742979d4c 🚀

---
+++ assets/vendor-BY9c7rfI.js
@@ -0,0 +1,2 @@
+let n="useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict",r=(t=21)=>{let e="",l=crypto.getRandomValues(new Uint8Array(t|=0));for(;t--;)e+=n[l[t]&63];return e};export{r as n};
+//# sourceMappingURL=vendor-BY9c7rfI.js.map
